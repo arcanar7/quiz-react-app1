41a5850fc503ff01cb0296af3f355f39147c610a
arcanar7 <arcanar7@gmail.com>
2019-09-13 16:17:34 +0400

    2

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import classes from './Drawer.css'
+import { NavLink } from 'react-router-dom'
+import Backdrop from '../../Ui/Backdrop/Backdrop'
 
-const links = [1, 2, 3]
+const links = [
+    { to: '/', label: 'Список', exact: true },
+    { to: '/auth', label: 'Авторизация', exact: false },
+    { to: '/quiz-creator', label: 'Создать тест', exact: false },
+]
 
 class Drawer extends Component {
+    clickHandler = () => {
+        this.props.onClose()
+    }
     renderLinks() {
         return links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a href>Link {link}</a>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={classes.active}
+                        onClick={this.clickHandler}
+                    >
+                        {link.label}
+                    </NavLink>
                 </li>
             )
         })
@@ -20,9 +36,12 @@ class Drawer extends Component {
             cls.push(classes.close)
         }
         return (
-            <nav className={cls.join(' ')}>
-                <ul>{this.renderLinks()}</ul>
-            </nav>
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>{this.renderLinks()}</ul>
+                </nav>
+                {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+            </React.Fragment>
         )
     }
 }
